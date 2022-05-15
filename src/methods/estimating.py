33d308f3b238a75e4d732500a1311e517c365695
franciscopfsimoes <francisco8857@gmail.com>
2022-05-15 16:17:11 +0200

    refactored estimating module

--- src/methods/estimating.py
+++ src/methods/estimating.py
@@ -3,22 +3,16 @@ import numpy as np
 import math
 from sklearn.linear_model import LinearRegression
 
-# Model specific algorithms
 import methods.sabr
 
-
 def foward(S, mu, T):
-
     f = S * math.exp(mu * T)
-
     return f
 
 
 def spot(f, mu, T):
     S = f * math.exp(-mu * T)
-
     return S
-
 
 ######################################Estimating alpha, rho and Vv############################
 
@@ -44,68 +38,43 @@ def ARV(beta, strike, fprice, duration, vol):
     Vvopt = 0
 
     for alpha in np.arange(alphalb, alphaub, alphastep):
-
         for rho in np.arange(rholb, rhoub, rhostep):
-
             for Vv in np.arange(Vvlb, Vvub, Vvstep):
-
                 sum = 0
-
                 for i in range(len(vol)):
-
                     est = SABR.impVol(
                         alpha, beta, rho, Vv, strike[i], fprice[i], duration[i]
                     )
 
                     dif = (est - vol[i]) ** 2
-
                     sum = sum + dif
-
                 if sum <= opt:
-
                     opt = sum
-
                     alphaopt = alpha
-
                     rhoopt = rho
-
                     Vvopt = Vv
-
     print(
         "your optimal parameters are alpha:", alphaopt, " rho:", rhoopt, " Vv:", Vvopt
     )
 
     opt = (alphaopt, rhoopt, Vvopt)
-
     return opt
 
 
-def B(
-    vol, fprice, S0K
-):  ########################################Estimating beta########################################
-
+def B(vol, fprice, S0K):  
+    ########################################Estimating beta########################################
     lvNtm = []  # log Near-The-Money volatility
-
     lfNtm = []  # log Near-The-Money foward price
-
     for i in range(len(vol)):
-
         if S0K[i] < 1.1:  # Our Near-The-Money condition (should be a bit tighter)
-
             lV = math.log(vol[i])
-
             lvNtm.append(lV)
-
             lF = math.log(fprice[i])
-
             lfNtm.append(lF)
 
     # prepare log(f) and log(vol) Near-the-money arrays for linear regression
     x = np.array(lfNtm).reshape((-1, 1))
     y = np.array(lvNtm)
-
     model = LinearRegression().fit(x, y)  # Linear regression
-
     beta = model.coef_ + 1  # beta as the slope of the linear regression model
-
     return beta
